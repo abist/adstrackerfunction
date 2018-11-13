4039298f7c3f2e5aaa77c9f7b479e63c1f3169bd
Aditya Bist <adbist@microsoft.com>
2018-11-13 14:03:12 -0800

    added app refresh timer trigger function

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -53,7 +53,17 @@ function getClosedIssues() {
   return getIssues(body("CLOSED"));
 }
 
+function triggerCommit() {
+  const url = "https://api.netlify.com/build_hooks/5beb44603813f032ca05fcb6";
+  const options = {
+      method: POST,
+      body: "{}"
+  };
+  return fetch(url, options).then(resp => resp.json());
+}
+
 module.exports = async function (context, myTimer) {
+    triggerCommit();
     var openIssues = await getOpenIssues();
     var closedIssues = await getClosedIssues();
   
